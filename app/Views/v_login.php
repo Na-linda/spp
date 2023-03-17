41ad3f4a85f6417cdabaa1dd5f66a43eb36143a6
Na-linda <lindaloveyou180@gmail.com>
2023-03-17 10:18:48 +0700

    linda spp

---
+++ app/Views/v_login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Aplikasi_Spp</title>
+	<meta charset="utf-8">
+	<link href="/css/bootstrap.css" rel="stylesheet"/>
+	<link href="/css/signin.css" rel="stylesheet"/>
+	
+  <link href="/fontawesome/css/all.min.css" rel="stylesheet"/>
+
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+	<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+</head>
+<body class="text-center">
+<div class="container">
+	<div class="row justify-content-center my-2">
+		<div class="col-lg-5 mx-5 border border-success rounded bg-info text-white p-5">
+			<h1 class="text-center"><b>Pembayaran_Spp</b></h1></br>
+<form class="form-signin" method="POST" action="/login">
+  <img class="mb-4" src="/logo-sekolah.jpeg" class="img-thumbnail">
+  <h1 class="h3 mb-3 font-weight-normal">Login Petugas</h1>
+  <label for="inputUser" class="sr-only">Username</label>
+  <input type="text" name="txtUsername" id="inputUser" class="form-control" placeholder="Username" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" name="txtPassword" id="inputPassword" class="form-control" placeholder="Password" required>
+  <button class="btn btn-lg btn-primary btn-block" type="submit"><i class="fas fa-sign-in-alt"></i> Login</button>
+</form>
+
+
+<body>
+</html>
